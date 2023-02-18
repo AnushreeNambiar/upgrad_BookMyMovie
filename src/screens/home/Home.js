88d777756ed47ecf27d721e2d70531a471b83006
Anushree <manushreen@gmail.com>
2023-02-18 14:48:32 +0530

    Header buttons

---
+++ src/screens/home/Home.js
@@ -0,0 +1,10 @@
+import Header from "../../common/header/Header";
+import React, { Component } from 'react';
+
+const Home = function(){
+    return(
+        <Header/>       
+    )
+}
+
+export default Home;
