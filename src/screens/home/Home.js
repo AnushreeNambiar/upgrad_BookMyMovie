a167b4daa8497dacdea45e2b809bc6175ee4b147
Anushree <manushreen@gmail.com>
2023-02-18 15:41:53 +0530

    Upcoming movies

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,15 @@
 import Header from "../../common/header/Header";
+import './Home.css';
 import React, { Component } from 'react';
 
 const Home = function(){
     return(
-        <Header/>       
+        <div>
+        <Header/>
+        <div className="movies-Heading" >
+            Upcoming Movies
+        </div>  
+        </div>     
     )
 }
 
