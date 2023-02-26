64b27e4be626196e7e78e74ebf41f41fc2af0db7
siddharthsharma <siddharth3.sharma@paytm.com>
2023-02-26 13:50:41 +0530

    Login button add

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,7 +1,55 @@
 import React, { Component } from "react";
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
 
+        }
+    }
+
+    openModal = () => {
+        this.setState({
+
+        });
+    }
+
+    closeModal = () => {
+        this.setState({
+
+        });
+    }
+
+    tabChangeHandler = (event, value) => {
+
+    }
+
+    loginClickHandler = () => {
+
+    }
+
+    registerClickHandler = () => {
+
+    }
+
+    logoutHandler = (error) => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="Movies App Logo" />
+                    <div className="login-button">
+                        <Button variant="contained" color="default" onClick={this.openModalHandler}>
+                            Login
+                        </Button>
+                    </div>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default Header;
